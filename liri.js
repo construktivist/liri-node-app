08082fd4cc9e5513d21761a802ac5da30b5d5d13
Keegan Kelly <keegankelly1@gmail.com>
2016-10-12 22:38:27 -0500

    Added function for spotify and OMDB queries

--- liri.js
+++ liri.js
@@ -6,7 +6,6 @@
 var twitter = require("twitter");
 var keys = require("./keys.js");
 var twitterKeys = keys.twitterAuth;
-var twitterQueryURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=KeeganKelly0&count=20";
 
 var client = new twitter({
   consumer_key: keys.twitterAuth.consumer_key,
@@ -14,6 +13,7 @@ var client = new twitter({
   access_token_key: keys.twitterAuth.access_token_key,
   access_token_secret: keys.twitterAuth.access_token_secret
 });
+
 
 function getMyTweets(){
 
@@ -39,6 +39,77 @@ function getMyTweets(){
 //============================================================================================================================================================================================================================
 //Twitter End
 
+//Spotify Start
+//============================================================================================================================================================================================================================
+function spotifySong(){
+	 var spotify = require('spotify');
+
+	 var songArray = process.argv;
+	 songArray.splice(0, 3);
+	 var songName = songArray.join(" ");
+	 
+	 spotify.search({ type: 'track', query: songName}, function(err, data) {
+	     if ( err ) {
+	         console.log('Error occurred: ' + err);
+	         return;
+	     }
+
+	     var song = data.tracks.items[0].name;
+	     var album = data.tracks.items[0].album.name;
+	     var artist = data.tracks.items[0].artists[0].name;
+	     var previewURL = data.tracks.items[0].preview_url;
+
+	     console.log("=======================================")
+	     console.log(" ")
+	     console.log("Song: " + song);
+	     console.log("Artist: " + artist);
+	     console.log("Album: " + album);
+	     console.log("Preview URL: " + previewURL);
+	     console.log(" ")
+	     console.log("=======================================")
+
+	 });
+
+};
+
+//============================================================================================================================================================================================================================
+//Spotify End
+
+//OMDB Start
+//============================================================================================================================================================================================================================
+function getMovie(){
+	var request = require('request');
+
+	var movieArray = process.argv;
+	 movieArray.splice(0, 3);
+	 var movieName = movieArray.join(" ");
+
+	request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json", function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    var movie = JSON.parse(body);
+	  }
+
+		console.log("=======================================")
+	    console.log(" ")
+	    console.log("Title: " + movie.Title);
+	    console.log("Year Released: " + movie.Released );
+	    console.log("IMDB Rating: " + movie.imdbRating);
+		console.log("Country: " + movie.Country);
+		console.log("Language: " + movie.Language);
+		console.log("Plot: " + movie.Plot);
+		console.log("Actors:  " + movie.Actors);
+		console.log("Rotten Tomatoes Rating: ");
+		console.log("Rotten Tomatoes URL: ");						
+	    console.log(" ")
+	    console.log("=======================================")
+
+	});
+
+};
+
+//============================================================================================================================================================================================================================
+//OMDB End
+
 var command = process.argv[2];
 
  switch(command){
@@ -46,12 +117,12 @@ var command = process.argv[2];
  		getMyTweets();
  		break;
 
- 	case "test1":
- 		console.log("test");
+ 	case "spotify-this-song":
+ 		spotifySong();
  		break;
 	
- 	case "test2":
- 		console.log("test");
+ 	case "movie-this":
+ 		getMovie();
  		break;
 
  	case "test3":
